bb66a2238a5124b6b235545706123ea6eb839021
Kay N <kaydigital.me@gmail.com>
2019-08-02 19:47:12 -0400

    separated NavLang component into its own directory

---
+++ src/components/nav-bar/nav-bar.component.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+import { fetchPopularRepos } from "../../utils/api";
+import NavLang from "../nav-lang/nav-lang.component";
+import ReposGrid from "../repos-grid/repos-grid.component";
+import "./nav-bar.styles.scss";
+
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All",
+      repos: {},
+      error: null
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+    this.isLoading = this.isLoading.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage,
+      error: null
+    });
+
+    if (!this.state.repos[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then(data =>
+          this.setState(({ repos }) => ({
+            repos: {
+              ...repos,
+              [selectedLanguage]: data
+            }
+          }))
+        )
+        .catch(error => {
+          console.warn("Error fetching repos", error);
+          this.setState({
+            error: "There was an error fetching the repositories."
+          });
+        });
+    }
+  }
+
+  isLoading() {
+    const { selectedLanguage, repos, error } = this.state;
+    return !repos[selectedLanguage] && error === null;
+  }
+
+  render() {
+    const { selectedLanguage, repos, error } = this.state;
+    return (
+      <React.Fragment>
+        <NavLang
+          selected={selectedLanguage}
+          onClickLang={this.updateLanguage}
+        />
+        {this.isLoading() && <p>LOADING</p>}
+        {error && <p>{error}</p>}
+        {repos[selectedLanguage] && (
+          <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>
+        )}
+      </React.Fragment>
+    );
+  }
+}
