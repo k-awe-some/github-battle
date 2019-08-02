5e970aca67a081329ad533ba4bee8d8cf6f1b8eb
Kay N <kaydigital.me@gmail.com>
2019-08-02 18:59:47 -0400

    added fetching request for github repos; fetched repos to ui on object level

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+
+import { fetchPopularRepos } from "../../utils/api";
 import "./navbar.styles.scss";
 
 export default class NavBar extends React.Component {
@@ -7,26 +9,56 @@ export default class NavBar extends React.Component {
     super(props);
 
     this.state = {
-      selectedLanguage: "All"
+      selectedLanguage: "All",
+      repos: null,
+      error: null
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
+    this.isLoading = this.isLoading.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
   }
 
   updateLanguage(selectedLanguage) {
     this.setState({
-      selectedLanguage
+      selectedLanguage,
+      repos: null,
+      error: null
     });
+
+    fetchPopularRepos(selectedLanguage)
+      .then(repos =>
+        this.setState({
+          repos,
+          error: null
+        })
+      )
+      .catch(error => {
+        console.warn("Error fetching repos", error);
+        this.setState({
+          error: "There was an errolr fetching the repositories."
+        });
+      });
+  }
+
+  isLoading() {
+    return this.state.repos === null && this.state.error === null;
   }
 
   render() {
-    const { selectedLanguage } = this.state;
+    const { selectedLanguage, repos, error } = this.state;
     return (
       <React.Fragment>
         <NavLang
           selected={selectedLanguage}
           onClickLang={this.updateLanguage}
         />
+        {this.isLoading() && <p>LOADING</p>}
+        {error && <p>{error}</p>}
+        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
       </React.Fragment>
     );
   }
