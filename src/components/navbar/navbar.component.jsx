fa5d2b5f1fa1f9b38d15b3b70a966db5f4b7d309
Kay N <kaydigital.me@gmail.com>
2019-08-02 17:42:27 -0400

    moved language nav into its own functional component

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,18 +1,52 @@
 import React from "react";
 import "./navbar.styles.scss";
 
-export default class Navbar extends React.Component {
-  render() {
-    const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      selectedLanguage: "All"
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    });
+  }
+
+  render() {
+    const { selectedLanguage } = this.state;
     return (
-      <ul className="nav-bar">
-        {languages.map((language, index) => (
-          <li key={index}>
-            <button className="nav-btn nav-link">{language}</button>
-          </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <NavLang
+          selected={selectedLanguage}
+          onClickLang={this.updateLanguage}
+        />
+      </React.Fragment>
     );
   }
 }
+
+const NavLang = ({ selected, onClickLang }) => {
+  const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
+
+  return (
+    <ul className="nav-lang">
+      {languages.map(language => (
+        <li key={language}>
+          <button
+            className="nav-btn nav-link"
+            style={language === selected ? { color: "rgb(187, 46, 31)" } : null}
+            onClick={() => onClickLang(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
