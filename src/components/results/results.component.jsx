383957d00fc31bb3cae8234ce8340445972c93cb
Kay N <kaydigital.me@gmail.com>
2019-08-03 18:57:44 -0400

    updated Results component to render results to ui on object level

--- src/components/results/results.component.jsx
+++ src/components/results/results.component.jsx
@@ -4,18 +4,42 @@ import { getResults } from "../../utils/api";
 import "./results.styles.scss";
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    };
+  }
+
   componentDidMount() {
     const { playerOne, playerTwo } = this.props;
-    getResults([playerOne, playerTwo]).then(players =>
-      console.log("data", players)
-    );
+
+    getResults([playerOne, playerTwo])
+      .then(players =>
+        this.setState({
+          winner: players[0],
+          loser: players[1],
+          error: null,
+          loading: false
+        })
+      )
+      .catch(({ message }) => {
+        this.setState({
+          error: message,
+          loading: false
+        });
+      });
   }
 
   render() {
     return (
       <div>
         Results
-        <pre>{JSON.stringify(this.props, null, 2)}</pre>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     );
   }
