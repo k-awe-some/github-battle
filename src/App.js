3a09a6fd9970dc94bef8083cb7db9dfc7788d12b
Kay N <kaydigital.me@gmail.com>
2019-08-05 10:23:13 -0400

    created ThemeProvider & ThemeConsumer with React.createContext() (to toggle light/dark mode)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from "react";
 import "./App.css";
 import Popular from "./pages/popular/popular.component";
 import Battle from "./pages/battle/battle.component";
+import { ThemeProvider } from "./contexts/theme";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(theme => ({
+          theme: theme === "light" ? "dark" : "light"
+        }));
+      }
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <Popular />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="App">
+          <Popular />
+        </div>
+      </ThemeProvider>
     );
   }
 }
