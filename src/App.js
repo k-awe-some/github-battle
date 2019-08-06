a8c60bbf11d6146d9d44a8476128d13ec556fd64
Kay N <kaydigital.me@gmail.com>
2019-08-06 16:36:28 -0400

    added 404 page with Switch component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Popular from "./pages/popular/popular.component";
 import Battle from "./pages/battle/battle.component";
 import Results from "./components/results/results.component";
 import { ThemeProvider } from "./contexts/theme";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   constructor(props) {
@@ -27,9 +27,13 @@ class App extends React.Component {
           <div className={this.state.theme}>
             <div className="App">
               <NavBar />
-              <Route exact path="/" component={Popular} />
-              <Route exact path="/battle" component={Battle} />
-              <Route path="/battle/results" component={Results} />
+
+              <Switch>
+                <Route exact path="/" component={Popular} />
+                <Route exact path="/battle" component={Battle} />
+                <Route path="/battle/results" component={Results} />
+                <Route render={() => <h2>404</h2>} />
+              </Switch>
             </div>
           </div>
         </ThemeProvider>
